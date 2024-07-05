d72f82e51c56067b65599f15746b46047523387b
ggboy <630591905@qq.com>
2024-07-06 00:27:43 +0800

    add unit_test

--- Mutex/mutex_test.go
+++ Mutex/mutex_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestMutex(t *testing.T) {
 	const numGoroutines = 1000
-	const numIncrements = 1000
+	const numIncrements = 10000
 
 	var counter int32
 	m := NewMutex()
@@ -35,10 +35,38 @@ func TestMutex(t *testing.T) {
 
 func TestSyncMutex(t *testing.T) {
 	const numGoroutines = 1000
-	const numIncrements = 1000
+	const numIncrements = 10000
 
 	var counter int32
 	m := sync.Mutex{}
+	var wg sync.WaitGroup
+
+	for i := 0; i < numGoroutines; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for j := 0; j < numIncrements; j++ {
+				m.Lock()
+				counter++
+				m.Unlock()
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	expected := int32(numGoroutines * numIncrements)
+	if counter != expected {
+		t.Errorf("expected counter to be %d, but got %d", expected, counter)
+	}
+}
+
+func TestSpinMutex(t *testing.T) {
+	const numGoroutines = 1000
+	const numIncrements = 10000
+
+	var counter int32
+	m := NewSpinLock()
 	var wg sync.WaitGroup
 
 	for i := 0; i < numGoroutines; i++ {
